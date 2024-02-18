27551fab7d417216f559310db306d1980a4ccbf0
beebrown <bruno.grosdesirs@outlook.fr>
2024-02-18 16:40:25 +0100

    maj rating stars

--- src/components/Location.js
+++ src/components/Location.js
@@ -8,6 +8,7 @@ function Location() {
   const params = useParams();
 
   const logement = logements.filter((e) => e.id === params.id);
+  const hostname = logement[0].host.name.split(" ");
 
   return (
     <div>
@@ -15,32 +16,39 @@ function Location() {
       <div className='main'>
         <div className='logement'>
           <div className='logement__carrousel'>
-
+            <img src={logement[0].pictures[0]} alt={logement[0].title} />
           </div>
           <div className='logement__infos'>
             <div className='logement_location'>
               <p className='logement__title'>{logement[0].title}</p>
-              <p className='logement__title'>{logement[0].location}</p>
+              <p className='logement__city'>{logement[0].location}</p>
               <div className='logement__tags'>
+                {logement[0].tags.map((tag) => {
+                  return <p>{tag}</p>
+                })}
               </div>
             </div>
             <div className='logement_rate'>
               <div className='logement__host'>
-                <p>{logement[0].host.name}</p>
+                <div className='logement__host__name'>
+                  <p>{hostname[0]}</p>
+                  <p>{hostname[1]}</p>
+                </div>
                 <img src={logement[0].host.picture} alt={logement[0].host.name} />
               </div>
               <div className='logement__host__rate'>
-                <p>{logement[0].rating}</p>
+                {() => {
+                  for (let i = 0; i < parseInt(logement[0].rating); i++) {
+                    <img src="../assets/star-active.svg" alt="rating star" />
+                    console.log(parseInt(logement[0].rating));
+                  }
+                }}
               </div>
             </div>
           </div>
           <div className='logement__data'>
-            <div className='logement_description'>
-
-            </div>
-            <div className='logement_equipement'>
-
-            </div>
+            <button className='logement__section'>Description</button>
+            <button className='logement__section'>Équipement</button>
           </div>
         </div>
       </div>
