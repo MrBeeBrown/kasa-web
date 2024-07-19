0c189970353efe443f41b5a589a283e9d041b45e
beebrown <bruno.grosdesirs@outlook.fr>
2024-07-19 22:32:35 +0200

    code for the carousel

---
+++ src/components/Carousel.js
@@ -0,0 +1,34 @@
+import '../styles/carousel.scss';
+import { useState } from 'react';
+
+
+function Carousel({ data }) {
+
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  const nextSlide = () => {
+    setCurrentSlide(currentSlide === data.length - 1 ? 0 : currentSlide + 1);
+  };
+  const prevSlide = () => {
+    setCurrentSlide(currentSlide === 0 ? data.length - 1 : currentSlide - 1);
+  };
+
+  return (
+    <div className='logement__carrousel'>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" className='chevron chevron_left' onClick={prevSlide}>
+        <path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256 246.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z" fill="#FFFFFF" />
+      </svg>
+      {data.map((item, index) => {
+        return <img src={item} alt={index} key={index} className={currentSlide === index ? 'active' : 'inactive'} />
+      })}
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" className='chevron chevron_right' onClick={nextSlide}>
+        <path d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z" fill="#FFFFFF" />
+      </svg>
+      <span className='indicators'>
+        {currentSlide + 1} / {data.length}
+      </span>
+    </div>
+  )
+}
+
+export default Carousel;
--- src/components/Location.js
+++ src/components/Location.js
@@ -1,6 +1,7 @@
 import Header from './Header';
 import Footer from './Footer';
 import logements from "../data/logements.json";
+import Carousel from "./Carousel";
 import { useParams } from 'react-router-dom';
 import { useState } from 'react';
 import '../styles/location.scss';
@@ -11,17 +12,30 @@ function Location() {
   const logement = logements.filter((e) => e.id === params.id);
   const hostname = logement[0].host.name.split(" ");
 
-  const [showItem, setShowItem] = useState(false);
+  const [showItemDescription, setShowItemDescription] = useState(true);
+  const [showItemEquipement, setShowItemEquipement] = useState(true);
 
-  const toggleShow = (e) => {
-    console.log(e.target);
-    setShowItem(!showItem);
+  const toggleShowDescription = () => {
+    setShowItemDescription(!showItemDescription);
+  }
+
+  const toggleShowEquipement = () => {
+    setShowItemEquipement(!showItemEquipement);
   }
 
   const ratingStars = () => {
     const stars = [];
     for (let i = 0; i < parseInt(logement[0].rating); i++) {
+      stars.push(<svg width="36" height="36" viewBox="0 0 36 36" fill="none" xmlns="http://www.w3.org/2000/svg" key={i}>
+        <path d="M19.7212 6.84366C19.4728 6.32809 18.9479 6 18.3714 6C17.7949 6 17.2747 6.32809 17.0216 6.84366L14.0079 13.0445L7.27748 14.0382C6.71506 14.1225 6.24636 14.5162 6.07295 15.0552C5.89953 15.5942 6.04014 16.1895 6.44321 16.5879L11.327 21.4201L10.174 28.2491C10.0803 28.8115 10.3146 29.3833 10.7786 29.7161C11.2426 30.0489 11.8566 30.0911 12.3628 29.8239L18.3761 26.6133L24.3894 29.8239C24.8956 30.0911 25.5096 30.0536 25.9736 29.7161C26.4376 29.3786 26.6719 28.8115 26.5782 28.2491L25.4205 21.4201L30.3043 16.5879C30.7073 16.1895 30.8526 15.5942 30.6745 15.0552C30.4964 14.5162 30.0324 14.1225 29.47 14.0382L22.7349 13.0445L19.7212 6.84366Z" fill="#FF6060" />
+      </svg>);
     }
+    while (stars.length < 5) {
+      stars.push(<svg width="36" height="36" viewBox="0 0 36 36" fill="none" xmlns="http://www.w3.org/2000/svg" key={stars.length}>
+        <path d="M19.7212 6.84366C19.4728 6.32809 18.9479 6 18.3714 6C17.7949 6 17.2747 6.32809 17.0216 6.84366L14.0079 13.0445L7.27748 14.0382C6.71506 14.1225 6.24636 14.5162 6.07295 15.0552C5.89953 15.5942 6.04014 16.1895 6.44321 16.5879L11.327 21.4201L10.174 28.2491C10.0803 28.8115 10.3146 29.3833 10.7786 29.7161C11.2426 30.0489 11.8566 30.0911 12.3628 29.8239L18.3761 26.6133L24.3894 29.8239C24.8956 30.0911 25.5096 30.0536 25.9736 29.7161C26.4376 29.3786 26.6719 28.8115 26.5782 28.2491L25.4205 21.4201L30.3043 16.5879C30.7073 16.1895 30.8526 15.5942 30.6745 15.0552C30.4964 14.5162 30.0324 14.1225 29.47 14.0382L22.7349 13.0445L19.7212 6.84366Z" fill="#E3E3E3" />
+      </svg>)
+    }
+    return stars;
   }
 
   return (
@@ -29,9 +43,7 @@ function Location() {
       <Header />
       <div className='main'>
         <div className='logement'>
-          <div className='logement__carrousel'>
-            <img src={logement[0].pictures[0]} alt={logement[0].title} />
-          </div>
+          <Carousel data={logement[0].pictures} />
           <div className='logement__infos'>
             <div className='logement_location'>
               <p className='logement__title'>{logement[0].title}</p>
@@ -51,11 +63,7 @@ function Location() {
                 <img src={logement[0].host.picture} alt={logement[0].host.name} />
               </div>
               <div className='logement__host__rate'>
-                {() => {
-                  for (let i = 0; i < parseInt(logement[0].rating); i++) {
-                    <img src="../assets/star-active.svg" alt="rating star" />
-                  }
-                }}
+                {ratingStars()}
               </div>
             </div>
           </div>
@@ -63,20 +71,20 @@ function Location() {
             <div className='description'>
               <div className='description__data'>
                 <button className='logement__section'>Description</button>
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className={showItem ? "rotate" : "icon"} onClick={(e) => toggleShow(e)}>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className={showItemDescription ? "rotate" : "icon"} onClick={() => toggleShowDescription()}>
                   <path fill="#FFFFFF" d="M233.4 105.4c12.5-12.5 32.8-12.5 45.3 0l192 192c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L256 173.3 86.6 342.6c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l192-192z" />
                 </svg>
               </div>
-              <p className={showItem ? 'show' : 'hidden'}>{logement[0].description}</p>
+              <p id='description' className={showItemDescription ? 'show' : 'hidden'}>{logement[0].description}</p>
             </div>
             <div className='equipement'>
               <div className='equipement__data'>
                 <button className='logement__section'>Équipement</button>
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className={showItem ? "rotate" : "icon"} onClick={(e) => toggleShow(e)}>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className={showItemEquipement ? "rotate" : "icon"} onClick={() => toggleShowEquipement()}>
                   <path fill="#FFFFFF" d="M233.4 105.4c12.5-12.5 32.8-12.5 45.3 0l192 192c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L256 173.3 86.6 342.6c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l192-192z" />
                 </svg>
               </div>
-              <p className={showItem ? 'show' : 'hidden'}>{logement[0].equipments.map((equipement) => { return <li key={equipement}>{equipement}</li> })}</p>
+              <p id='equipement' className={showItemEquipement ? 'show' : 'hidden'}>{logement[0].equipments.map((equipement) => { return <li key={equipement}>{equipement}</li> })}</p>
             </div>
           </div>
         </div>
